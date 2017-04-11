e90261a6f6ee42d0ece649ae459c4dac6f408ca3
Alexander Chan <alexanderchan97@gmail.com>
2017-04-10 21:13:45 -0400

    Add transform matrix methods

--- scene/transform.h
+++ scene/transform.h
@@ -3,12 +3,19 @@
 
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
 
 class Transform {
 public:
+	glm::mat4 transMat();
+	glm::mat4 invTransMat();
+
 	glm::vec3 translation;
 	glm::vec3 rotation;
 	glm::vec3 scale;
+
+	glm::mat4 transform;
+	glm::mat4 invTransform;
 };
 
 #endif // __TRANSFORM_H__
