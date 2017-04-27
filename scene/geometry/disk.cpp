9e30e8faf0aa483d1483d91130df84bf681d6481
Dongho Kim <Dongho Kim>
2017-04-27 18:40:50 -0400

    disk added

---
+++ scene/geometry/disk.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tinyobj/tiny_obj_loader.h>
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/vec3.hpp>
+#include <math.h>
+#include <scene/geometry/geometry.h>
+#include <scene/geometry/disk.h>
+#include <scene/scene.h>
+
+Disk::Disk(QString name, Transform transform, Material *material) {
+    this->s = glm::vec3(0.5, 0.5, 0);
+    this->name = name;
+    this->transform = transform;
+    this->material = material;
+}
+
+Disk::~Disk() {}
+
+float Disk::getT(glm::vec3 n, glm::vec3 s, glm::vec3 origin, glm::vec3 direction) const {
+    float resultTop = glm::dot(n, s - origin);
+    float resultBot = glm::dot(n, direction);
+    float result = resultTop/resultBot;
+    return result;
+}
+
+bool Disk::intersect(const Ray &ray, Intersection *intersection) const {
+    Ray rayMod = ray.getTransformedCopy(transform.invTransform);
+    glm::vec3 rayOriMod = rayMod.origin;
+    glm::vec3 rayDirMod = rayMod.direction;
+    glm::vec3 n = glm::vec3(0, 0, 1);
+    glm::vec3 center = glm::vec3(0, 0, 0);
+    intersection->t = getT(n, glm::vec3(0.5, 0.5, 0), rayOriMod, rayDirMod);
+    glm::vec3 isectInit = rayOriMod + intersection->t*rayDirMod;
+    glm::vec3 d = isectInit - center;
+    float dd = glm::dot(d, d);
+    if (sqrt(dd) <= 0.5) {
+        intersection->objectHit = this;
+        intersection->isectPoint = transform.transform * glm::vec4(isectInit, 1);
+        glm::vec3 normalInit = glm::vec3(0, 0, 1);
+        intersection->normal = transform.invTransTrans * glm::vec4(normalInit, 0);
+        return true;
+    }
+    return false;
+}
+
+void Disk::mapNormal(Intersection &isect) const {
+    if (!(isect.objectHit->material->normalMap == nullptr)) {
+        glm::vec3 mapNorm = spInterpolation(isect, material->normalMap);
+        mapNorm = (mapNorm * glm::vec3(255) - glm::vec3(128)) / glm::vec3(128);
+        isect.normal = transform.invTransTrans * glm::vec4(mapNorm, 0);
+    }
+}
+
+glm::vec3 Disk::getColor(Intersection &isect) const {
+    if (isect.objectHit->material->texture == nullptr) {
+        return isect.objectHit->material->baseColor;
+    }
+
+    QImage *tex = this->material->texture;
+    return spInterpolation(isect, tex);
+}
+
+glm::vec3 Disk::spInterpolation(Intersection &isect, QImage *attrib) const {
+    glm::vec3 isecA = transform.invTransform * glm::vec4(isect.isectPoint, 1);
+    float u = isecA[0]+0.5;
+    float v = isecA[1]+0.5;
+
+    float w = attrib->width() - 1;
+    float h = attrib->height() - 1;
+    u = u < 0.5 ? u + 2 * (0.5 - u) : u - 2 * (u - 0.5);
+    float convU = w*u;
+    float convV = h*v;
+    QRgb first = slerp(getAlpha(convU, ceil(convU), floor(convU)), attrib->pixel(QPoint(ceil(convU), ceil(convV))), attrib->pixel(QPoint(floor(convU), ceil(convV))));
+    QRgb second = slerp(getAlpha(convU, ceil(convU), floor(convU)), attrib->pixel(QPoint(ceil(convU), floor(convV))), attrib->pixel(QPoint(floor(convU), floor(convV))));
+    QRgb final = slerp(getAlpha(convV, ceil(convV), floor(convV)), first, second);
+    glm::vec3 ret = glm::vec3(qRed(final), qGreen(final), qBlue(final));
+    return ret / glm::vec3(255);
+}
+
+float Disk::getAlpha(float y, float py, float qy) const {
+    float result = ((y-py)/(qy-py));
+    return result;
+}
+
+QRgb Disk::slerp(float alpha, QRgb az, QRgb bz) const {
+    float resultR = (1-alpha)*qRed(az) + alpha*qRed(bz);
+    float resultG = (1-alpha)*qGreen(az) + alpha*qGreen(bz);
+    float resultB = (1-alpha)*qBlue(az) + alpha*qBlue(bz);
+    return qRgb(resultR, resultG, resultB);
+}
+
