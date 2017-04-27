6b85e721e56458baaaed8fecff3cd7d926debe58
Dongho Kim <Dongho Kim>
2017-04-27 12:33:38 -0400

    cylinder height limited

--- scene/geometry/cylinder.cpp
+++ scene/geometry/cylinder.cpp
@@ -16,7 +16,7 @@ Cylinder::Cylinder(QString name, Transform transform, Material *material) {
 
 Cylinder::~Cylinder() {}
 
-float Cylinder::getT(float a, float b, float c) const {
+float Cylinder::getT0(float a, float b, float c) const {
     float result = ((-1*b) -sqrt((b*b)-(4*a*c)))/(2*a);
     if (result < 0) {
         result = ((-1*b) +sqrt((b*b)-(4*a*c)))/(2*a);
@@ -32,18 +32,58 @@ bool Cylinder::intersect(const Ray &ray, Intersection *intersection) const {
     float a = (rayDirMod[0]*rayDirMod[0]) + (rayDirMod[2]*rayDirMod[2]);
     float b = 2*((rayDirMod[0]*rayOriMod[0]) + (rayDirMod[2]*rayOriMod[2]));
     float c = (rayOriMod[0]*rayOriMod[0]) + (rayOriMod[2]*rayOriMod[2]) - (radius*radius);
-    float discriminant = (b*b)-(4*a*c);
-    if (discriminant < 0) {
+    //float discriminant = (b*b)-(4*a*c);
+    //if (discriminant < 0) {
+   //     return false;
+    //} else {
+    float t0 = ((-1*b) -sqrt((b*b)-(4*a*c)))/(2*a);
+    float t1 = ((-1*b) +sqrt((b*b)-(4*a*c)))/(2*a);
+    if (t1 < t0) {
+        float temp = t0;
+        t0 = t1;
+        t1 = temp;
+    }
+    float y0 = rayOriMod[1]+(t0*rayDirMod[1]);
+    float y1 = rayOriMod[1]+(t1*rayDirMod[1]);
+
+    if ((y0 > 0.5) && (y1 > 0.5)) {
         return false;
-    } else {
-        intersection->t = getT(a, b, c);
+    } else if ((y0 > 0.5) && (y1 < 0.5)) {
+        intersection->t = t0;
         intersection->objectHit = this;
         glm::vec3 isectInit = rayOriMod + intersection->t*rayDirMod;
         intersection->isectPoint = transform.transform * glm::vec4(isectInit, 1);
         glm::vec3 normalInit = glm::normalize(glm::vec3(isectInit.x, isectInit.y, isectInit.z));
         intersection->normal = transform.invTransTrans * glm::vec4(normalInit, 0);
         return true;
+    } else if ((y0 < 0.5) && (y0 > -0.5)) {
+        intersection->t = t0;
+        intersection->objectHit = this;
+        glm::vec3 isectInit = rayOriMod + intersection->t*rayDirMod;
+        intersection->isectPoint = transform.transform * glm::vec4(isectInit, 1);
+        glm::vec3 normalInit = glm::normalize(glm::vec3(isectInit.x, isectInit.y, isectInit.z));
+        intersection->normal = transform.invTransTrans * glm::vec4(normalInit, 0);
+        return true;
+    } else if ((y0 < -0.5) && (y1 > -0.5)) {
+        intersection->t = t0;
+        intersection->objectHit = this;
+        glm::vec3 isectInit = rayOriMod + intersection->t*rayDirMod;
+        intersection->isectPoint = transform.transform * glm::vec4(isectInit, 1);
+        glm::vec3 normalInit = glm::normalize(glm::vec3(isectInit.x, isectInit.y, isectInit.z));
+        intersection->normal = transform.invTransTrans * glm::vec4(normalInit, 0);
+        return true;
+    } else {
+        return false;
     }
+
+     /*   intersection->t = getT(a, b, c);
+        intersection->objectHit = this;
+        glm::vec3 isectInit = rayOriMod + intersection->t*rayDirMod;
+        intersection->isectPoint = transform.transform * glm::vec4(isectInit, 1);
+        glm::vec3 normalInit = glm::normalize(glm::vec3(isectInit.x, isectInit.y, isectInit.z));
+        intersection->normal = transform.invTransTrans * glm::vec4(normalInit, 0);
+        return true;*/
+
 }
 
 void Cylinder::mapNormal(Intersection &isect) const {
