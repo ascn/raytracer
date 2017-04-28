925d2983e78930da2bb4cfa16cb6e29b6eea0339
Dongho Kim <Dongho Kim>
2017-04-27 22:13:15 -0400

    hyperboloid

---
+++ scene/geometry/hyperboloid.h
@@ -0,0 +1,20 @@
+#ifndef HYPERBOLOID_H
+#define HYPERBOLOID_H
+
+#include <scene/geometry/geometry.h>
+#include <glm/vec3.hpp>
+#include <QColor>
+
+class Hyperboloid : public Geometry {
+public:
+    Hyperboloid(QString name, Transform transform, Material *material);
+    ~Hyperboloid();
+    bool intersect(const Ray &ray, Intersection *intersection) const;
+    void mapNormal(Intersection &isect) const;
+    glm::vec3 getColor(Intersection &isect) const;
+    float getAlpha(float y, float py, float qy) const;
+    QRgb slerp(float alpha, QRgb az, QRgb bz) const;
+    glm::vec3 sphereInterpolation(Intersection &isect, QImage *attrib) const;
+};
+
+#endif // HYPERBOLOID_H
